cea563b9842a2c576b17bc7f7998eca262e04c6d
Lubna Hajhussein <hajhussein.lubna@gmail.com>
2024-12-16 22:21:43 +0300

    fixes

--- src/components/shared/Website/Templates/Docs/1inchClone/Route1inchClone.js
+++ src/components/shared/Website/Templates/Docs/1inchClone/Route1inchClone.js
@@ -1,12 +1,11 @@
 import TemplatePage from "../../../../../sharedUI/TemplatePage/TemplatePage";
-import demo from "./../../../../../../assets/demo/1inch_clone.mp4";
 
 function Route1inchClone() {
   return (
     <TemplatePage
       srcCode={"https://github.com/Eng-Lubna-Hajhussein/1inch-clone"}
       livePreview={"https://eng-lubna-hajhussein.github.io/1inch-clone/"}
-      demo={demo}
+      demo={"https://imgur.com/5KYqrze.mp4"}
       title="1inch Clone"
       description={
         "A responsive template inspired by the popular 1inch cryptocurrency exchange aggregator. Perfect for creating a decentralized exchange or finance app interface. The layout is fully responsive, adapting seamlessly across desktop, tablet, and mobile devices for an optimal user experience."
--- src/components/shared/Website/Templates/Docs/adminDahboard/RouteAdminDashboard.js
+++ src/components/shared/Website/Templates/Docs/adminDahboard/RouteAdminDashboard.js
@@ -1,12 +1,11 @@
 import TemplatePage from "../../../../../sharedUI/TemplatePage/TemplatePage";
-import demo from "./../../../../../../assets/demo/admin_dashboard.mp4";
 
 function RouteAdminDashboard() {
   return (
     <TemplatePage
       srcCode={"https://github.com/Eng-Lubna-Hajhussein/admin-dashboard"}
       livePreview={"https://eng-lubna-hajhussein.github.io/admin-dashboard/"}
-      demo={demo}
+      demo={"https://imgur.com/60vKr5M.mp4"}
       title="Admin Dashboard"
       description={
         "A comprehensive admin dashboard template with a wide range of components, perfect for managing data and analytics for web applications."
